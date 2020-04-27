d59c926682cf3a09fdb43264fbe76faf773dea3d
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-04-27 13:54:06 +0700

    :construction: setup data users

--- lib/src/blocs/splash_bloc.dart
+++ lib/src/blocs/splash_bloc.dart
@@ -1,14 +1,36 @@
+import 'package:flutter_local_db/src/data/hive/company_hive.dart';
+import 'package:flutter_local_db/src/data/hive/user_hive.dart';
+import 'package:flutter_local_db/src/data/hive_constants.dart';
 import 'package:flutter_local_db/src/models/user_model.dart';
 import 'package:flutter_local_db/src/repositories/app_repository.dart';
+import 'package:hive/hive.dart';
 
 class SplashBloc {
 
   final AppRepository _appRepository = AppRepository();
 
   Future<bool> getUsers() async {
+    Box<UserHive> boxUsers = await Hive.openBox<UserHive>(USERS);
+    boxUsers.clear();
     try {
       List<User> users = await _appRepository.getUser();
       print('Splash Bloc # users count : ${users.length}');
+      users.forEach((User user){
+        UserHive userHive = UserHive(
+          id: user.id,
+          name: user.name,
+          username: user.username,
+          email: user.email,
+          phone: user.phone,
+          website: user.website,
+          company: CompanyHive(
+            name: user.company.name,
+            catchPhrase: user.company.catchPhrase,
+            bs: user.company.bs
+          )
+        );
+        boxUsers.add(userHive);
+      });
       return true;
     } catch (err) {
       print(err.toString());
