fab596c3fa91acad82bcf32db5819b3511b405b5
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-04-27 12:15:17 +0700

    :pencil: init Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,20 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_local_db/src/my_app.dart';
+import 'dart:io';
 
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_local_db/src/my_app.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  initHive();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_){
+    runApp(MyApp());
+  });
+}
+
+void initHive() async {
+  final Directory appDirectory = await getApplicationDocumentsDirectory();
+  Hive.init(appDirectory.path);
+}
