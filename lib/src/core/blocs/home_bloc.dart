0fd6f02c1e9834b0a5eb1028d884edc6ad793231
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-01-24 18:18:21 +0700

    :pencil: load user from db

--- lib/src/core/blocs/home_bloc.dart
+++ lib/src/core/blocs/home_bloc.dart
@@ -1,7 +1,9 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_local_db/src/core/data/hive_constants.dart';
 import 'package:flutter_local_db/src/core/events/home_event.dart';
 import 'package:flutter_local_db/src/core/models/user_model.dart';
 import 'package:flutter_local_db/src/core/states/home_state.dart';
+import 'package:hive/hive.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   
@@ -9,20 +11,20 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
 
   @override
   Stream<HomeState> mapEventToState(HomeEvent event) async*{
-    print("HomeBloc # event is : $event");
     if (event is HomeEventInit) {
       yield _loadUser();
     }
   }
 
   HomeState _loadUser() {
-    print("Home Bloc # try load user");
     try {
       List<User> users = List<User>();
-      users.add(User(id: 1, name: "Yusuf", username: "yoesuv"));
+      var box = Hive.box<User>(USERS);
+      box.values.forEach((boxUser) {
+        users.add(boxUser);
+      });
       return HomeStateSuccess(listUser: users);
     } catch (e) {
-      print("Home Bloc # failed load hive users $e");
       return HomeStateFailed();
     }
   }
