3b506cb08c962eb94437d19e0c0e9ef822fbd4c2
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-01-24 09:26:21 +0700

    :newspaper: separated class model

--- lib/src/core/models/user_model.dart
+++ lib/src/core/models/user_model.dart
@@ -1,3 +1,6 @@
+import 'address_model.dart';
+import 'company_model.dart';
+
 class User {
 
   int id;
@@ -51,82 +54,6 @@ class User {
 
   static List<User> buildListFromJson(List<dynamic> json) {
     return json.map((dynamic x) => User.fromJson(x)).toList();
-  }
-
-}
-
-class Address {
-
-  String street;
-  String suite;
-  String city;
-  String zipcode;
-  Geo geo;
-
-  Address({this.street, this.suite, this.city, this.zipcode, this.geo});
-
-  Address.fromJson(Map<String, dynamic> json) {
-    street = json['street'];
-    suite = json['suite'];
-    city = json['city'];
-    zipcode = json['zipcode'];
-    geo = json['geo'] != null ? new Geo.fromJson(json['geo']) : null;
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['street'] = this.street;
-    data['suite'] = this.suite;
-    data['city'] = this.city;
-    data['zipcode'] = this.zipcode;
-    if (this.geo != null) {
-      data['geo'] = this.geo.toJson();
-    }
-    return data;
-  }
-
-}
-
-class Geo {
-
-  String lat;
-  String lng;
-
-  Geo({this.lat, this.lng});
-
-  Geo.fromJson(Map<String, dynamic> json) {
-    lat = json['lat'];
-    lng = json['lng'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['lat'] = this.lat;
-    data['lng'] = this.lng;
-    return data;
-  }
-}
-
-class Company {
-
-  String name;
-  String catchPhrase;
-  String bs;
-
-  Company({this.name, this.catchPhrase, this.bs});
-
-  Company.fromJson(Map<String, dynamic> json) {
-    name = json['name'];
-    catchPhrase = json['catchPhrase'];
-    bs = json['bs'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['name'] = this.name;
-    data['catchPhrase'] = this.catchPhrase;
-    data['bs'] = this.bs;
-    return data;
   }
 
 }
