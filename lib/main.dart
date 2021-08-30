da629b48932f7d1ce1a3d4a60563a4c2a7f0c066
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-08-30 13:39:23 +0700

    :hammer: fix init hive

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,20 @@ import 'package:flutter_local_db/src/core/models/geo_model.dart';
 import 'package:flutter_local_db/src/core/models/user_model.dart';
 import 'package:flutter_local_db/src/my_app.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart';
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  initHive();
+  await initHive();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_){
     runApp(MyApp());
   });
 }
 
-void initHive() async {
+Future<void> initHive() async {
   final Directory appDirectory = await getApplicationDocumentsDirectory();
-  Hive.init(appDirectory.path);
+  await Hive.initFlutter(appDirectory.path);
   Hive.registerAdapter(CompanyAdapter());
   Hive.registerAdapter(UserAdapter());
   Hive.registerAdapter(AddressAdapter());
