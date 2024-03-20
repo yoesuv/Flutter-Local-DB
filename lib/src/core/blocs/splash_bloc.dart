5414c1ac7e6bf7b140512bd5a89b2ab6fe311843
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-03-20 14:32:44 +0700

    :pencil: insert isar

--- lib/src/core/blocs/splash_bloc.dart
+++ lib/src/core/blocs/splash_bloc.dart
@@ -1,13 +1,12 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_local_db/src/core/data/hive_constants.dart';
 import 'package:flutter_local_db/src/core/events/splash_event.dart';
 import 'package:flutter_local_db/src/core/models/user_model.dart';
 import 'package:flutter_local_db/src/core/repositories/app_repository.dart';
 import 'package:flutter_local_db/src/core/states/splash_state.dart';
-import 'package:hive/hive.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
 class SplashBloc extends Bloc<SplashEvent, SplashState> {
-
   final AppRepository _appRepository = AppRepository();
 
   SplashBloc() : super(SplashStateInit()) {
@@ -15,17 +14,21 @@ class SplashBloc extends Bloc<SplashEvent, SplashState> {
   }
 
   void _getListUser(SplashEventInit event, Emitter<SplashState> emit) async {
-    Box<User> boxUsers = await Hive.openBox<User>(USERS);
+    final dir = await getApplicationDocumentsDirectory();
+    final isar = await Isar.open([UserSchema], directory: dir.path);
     try {
       List<User> users = await _appRepository.getUser();
-      await boxUsers.clear();
-      for (var user in users) {
-        await boxUsers.add(user);
-      }
+      await isar.writeTxn(() async {
+        await isar.clear();
+        await isar.users.putAll(users);
+      });
       emit(SplashStateSuccess(listUser: users));
     } catch (e) {
-      emit(SplashStateFailed(e as Exception));
+      if (e is Exception) {
+        emit(SplashStateFailed(e));
+      } else {
+        emit(SplashStateFailed(Exception()));
+      }
     }
   }
-
 }
