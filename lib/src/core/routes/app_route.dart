650f0b8f431dcd132a92b603dd1a5031bda3d368
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-03-23 16:08:19 +0700

    :fire: remove unused home resources

--- lib/src/core/routes/app_route.dart
+++ lib/src/core/routes/app_route.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_local_db/src/core/blocs/home_bloc.dart';
 import 'package:flutter_local_db/src/core/blocs/splash_bloc.dart';
 import 'package:flutter_local_db/src/core/models/user_model.dart';
 import 'package:flutter_local_db/src/ui/screens/detail.dart';
@@ -8,44 +7,29 @@ import 'package:flutter_local_db/src/ui/screens/home.dart';
 import 'package:flutter_local_db/src/ui/screens/splash.dart';
 
 class AppRoute {
-
   static Route<dynamic> routes(RouteSettings settings) {
     if (settings.name == '/') {
-      return MaterialPageRoute(
-        builder: (context) {
-          return BlocProvider(
-              create: (context) => SplashBloc(),
-              child: const Splash()
-          );
-        }
-      );
+      return MaterialPageRoute(builder: (context) {
+        return BlocProvider(
+            create: (context) => SplashBloc(), child: const Splash());
+      });
     } else if (settings.name == Home.routeName) {
-      return MaterialPageRoute(
-          builder: (context) {
-            return BlocProvider(
-                create: (context) => HomeBloc(),
-                child: const Home()
-            );
-          }
-      );
+      return MaterialPageRoute(builder: (context) {
+        return const Home();
+      });
     } else if (settings.name == Detail.routeName) {
       final User user = settings.arguments as User;
-      return MaterialPageRoute(
-          builder: (context) {
-            return Detail(user);
-          }
-      );
+      return MaterialPageRoute(builder: (context) {
+        return Detail(user);
+      });
     } else {
-      return MaterialPageRoute(
-        builder: (context) {
-          return const Scaffold(
-            body: Center(
-              child: Text('Page Not Found'),
-            ),
-          );
-        }
-      );
+      return MaterialPageRoute(builder: (context) {
+        return const Scaffold(
+          body: Center(
+            child: Text('Page Not Found'),
+          ),
+        );
+      });
     }
   }
-
 }
