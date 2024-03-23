a0bc606e2715f68b2581031e9929c16141c1ab3b
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-03-23 11:05:13 +0700

    :pencil: save data users

--- lib/src/my_app_bloc.dart
+++ lib/src/my_app_bloc.dart
@@ -1,14 +1,32 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_local_db/src/core/repositories/db_user_repository.dart';
 import 'package:flutter_local_db/src/my_app_event.dart';
 import 'package:flutter_local_db/src/my_app_state.dart';
+import 'package:formz/formz.dart';
 
 class MyAppBloc extends Bloc<MyAppEvent, MyAppState> {
+  final _dbUserRepository = DbUserRepository();
+
   MyAppBloc() : super(const MyAppState()) {
     on<MyAppInitUserEvent>(_onInitUser);
   }
 
   void _onInitUser(
-      MyAppInitUserEvent event,
+    MyAppInitUserEvent event,
     Emitter<MyAppState> emit,
-  ) {}
+  ) async {
+    emit(state.copyWith(
+      statusInsetUser: FormzSubmissionStatus.inProgress,
+    ));
+    try {
+      await _dbUserRepository.saveData(event.users);
+      emit(state.copyWith(
+        statusInsetUser: FormzSubmissionStatus.success,
+      ));
+    } catch (e) {
+      emit(state.copyWith(
+        statusInsetUser: FormzSubmissionStatus.failure,
+      ));
+    }
+  }
 }
